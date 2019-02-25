bd96cabbd2ce487c39947678a75efb26a18ce237
Dave van Herten <dvanherten@outlook.com>
2019-02-24 21:44:37 -0500

    Add functions for get and creation of quotes

--- DemoPennyPinchingServerless/Functions.cs
+++ DemoPennyPinchingServerless/Functions.cs
@@ -1,10 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
 
 namespace DemoPennyPinchingServerless.Functions
 {
     public class Functions
     {
+        public const string TemporaryPartitionKey = "Quote";// Probably http context User Id after Auth
+        public const string BaseRoute = "quote";
+        public const string StorageConnectionName = "AzureWebJobsStorage";
+
+        [FunctionName("GetQuotes")]
+        public static async Task<IActionResult> GetQuotes(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = BaseRoute)]
+            HttpRequest req,
+            [Table("quotes", Connection = StorageConnectionName)]
+            CloudTable quoteTable,
+            ILogger log)
+        {
+            log.LogInformation("Getting quote items");
+            var query = new TableQuery<QuoteTableEntity>().Where(
+                TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, TemporaryPartitionKey)
+            );
+            var segment = await quoteTable.ExecuteQuerySegmentedAsync(query, null);
+            return new OkObjectResult(segment.Results.Select(QuoteMappings.ToQuote));
+        }
+
+        [FunctionName("CreateQuote")]
+        public static async Task<IActionResult> CreateQuote(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = BaseRoute)]
+            HttpRequest req,
+            [Table("quotes", Connection = StorageConnectionName)]
+            IAsyncCollector<QuoteTableEntity> quoteTable,
+            ILogger log)
+        {
+            log.LogInformation("Creating a new quote list item");
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var input = JsonConvert.DeserializeObject<QuoteCreateModel>(requestBody);
+
+            var quote = new Quote
+            {
+                WhoSaidIt = input.WhoSaidIt,
+                WhatDidTheySay = input.WhatDidTheySay
+            };
+            await quoteTable.AddAsync(quote.ToTableEntity(TemporaryPartitionKey));
+            return new OkObjectResult(quote);
+        }
     }
 }
--- DemoPennyPinchingServerless/Models.cs
+++ DemoPennyPinchingServerless/Models.cs
@@ -1,7 +1,50 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DemoPennyPinchingServerless
 {
+    public class Quote
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("n");
+        public string WhoSaidIt { get; set; }
+        public string WhatDidTheySay { get; set; }
+    }
+
+    public class QuoteCreateModel
+    {
+        public string WhoSaidIt { get; set; }
+        public string WhatDidTheySay { get; set; }
+    }
+
+    public class QuoteTableEntity : TableEntity
+    {
+        public string WhoSaidIt { get; set; }
+        public string WhatDidTheySay { get; set; }
+    }
+
+    public static class QuoteMappings
+    {
+        public static QuoteTableEntity ToTableEntity(this Quote Quote, string partitionKey)
+        {
+            return new QuoteTableEntity
+            {
+                PartitionKey = partitionKey,
+                RowKey = Quote.Id,
+                WhoSaidIt = Quote.WhoSaidIt,
+                WhatDidTheySay = Quote.WhatDidTheySay
+            };
+        }
+
+        public static Quote ToQuote(this QuoteTableEntity Quote)
+        {
+            return new Quote
+            {
+                Id = Quote.RowKey,
+                WhoSaidIt = Quote.WhoSaidIt,
+                WhatDidTheySay = Quote.WhatDidTheySay
+            };
+        }
+    }
 }
